d0f1ba3b47ca0fa2e206eefd89b5c351c61a6148
hugoortegaceu <hugo.ortega2@isepceu.es>
2024-11-21 13:57:38 -0500

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+#modulos_utiles/calculadora.py
+class Calculadora:
+    """
+    Clase Calculadora Básica: realiza operaciones matemáticas básicas.
+    """
+    def suma(self, a, b):
+        """Devuelve la suma de dos números."""
+        return a + b
+ 
+    def resta(self, a, b):
+        """Devuelve la resta de dos números."""
+        return a - b
+ 
+    def multiplicacion(self, a, b):
+        """Devuelve la multiplicación de dos números."""
+        return a * b
+ 
+    def division(self, a, b):
+        """
+        Devuelve la división de dos números.
+        Maneja la división por cero.
+        """
+        if b == 0:
+            raise ValueError("No se puede dividir por cero.")
+        return a / b
